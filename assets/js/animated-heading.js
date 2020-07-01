5a97bede6d0a3c537c0acbadec8b0180e7adaf85
Kushal Shekhar Pradhan <kushal.shekhar@hotmail.com>
2020-07-01 22:33:53 +0530

    Added animated heading,few other changes

---
+++ assets/js/animated-heading.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($) {
+    var animationDelay = 2400;
+  
+  animateHeadline($('.cd-headline'));
+  
+  function animateHeadline($headlines) {
+     $headlines.each(function(){
+        var headline = $(this);
+        //trigger animation
+        setTimeout(function(){ hideWord( headline.find('.is-visible') ) }, animationDelay);
+        //other checks here ...
+     });
+  }
+    
+    function hideWord($word) {
+     var nextWord = takeNext($word);
+     switchWord($word, nextWord);
+     setTimeout(function(){ hideWord(nextWord) }, animationDelay);
+  }
+  
+  function takeNext($word) {
+     return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+  }
+  
+  function switchWord($oldWord, $newWord) {
+     $oldWord.removeClass('is-visible').addClass('is-hidden');
+     $newWord.removeClass('is-hidden').addClass('is-visible');
+  }
+  });
+  
